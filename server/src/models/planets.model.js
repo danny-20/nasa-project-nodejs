1c3c1347e4a4e212fc89f87737d1b6b39fcc4332
danny-20 <57652772+danny-20@users.noreply.github.com>
2023-09-26 12:40:03 +0530

    load data on startup

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,43 @@
-const planets = [];
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
 
-module.expports = planets;
+const habitablePlanets = [];
+
+function ishabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  ); //koi_insol = insolation or stellar flux for temperature
+}
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (ishabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(`Planets found ${habitablePlanets.length}`);
+        resolve();
+      });
+  });
+}
+
+module.exports = { loadPlanetsData, planets: habitablePlanets };
